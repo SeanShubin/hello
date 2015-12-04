32108866b6b52992862d0cd48dc29abb536454bc
Sean Shubin <sshubin@cj.com>
2015-12-04 11:19:51 -0800

    cleanup

--- src/test/scala/com/seanshubin/hello/EmitStub.scala
+++ src/test/scala/com/seanshubin/hello/EmitStub.scala
@@ -2,7 +2,7 @@ package com.seanshubin.hello
 
 import scala.collection.mutable.ArrayBuffer
 
-class EmitStub extends ((String) => Unit) {
+class EmitStub extends (String => Unit) {
   val emitted: ArrayBuffer[String] = new ArrayBuffer
 
   override def apply(line: String): Unit = emitted.append(line)
