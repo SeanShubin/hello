dd42fdaf2ef920c8f6944310ed8ed3e31fe77a45
Sean Shubin <sshubin@cj.com>
2017-06-28 11:39:31 -0700

    refactored files stub

--- src/test/scala/com/seanshubin/hello/ApplicationBehaviorTest.scala
+++ src/test/scala/com/seanshubin/hello/ApplicationBehaviorTest.scala
@@ -1,7 +1,5 @@
 package com.seanshubin.hello
 
-import java.nio.charset.StandardCharsets
-
 import org.scalatest.FunSuite
 
 class ApplicationBehaviorTest extends FunSuite {
@@ -9,7 +7,7 @@ class ApplicationBehaviorTest extends FunSuite {
     //given
     val args = Seq("configuration.txt")
     val clock = new ClockStub(1000, 1789)
-    val files = new FilesStub(Map("configuration.txt" -> "world"), StandardCharsets.UTF_8)
+    val files = new FilesStub(Map("configuration.txt" -> "world"))
     val emit = new EmitStub
     val applicationBehavior = new ApplicationBehavior(args, clock, files, emit)
 
--- src/test/scala/com/seanshubin/hello/FilesStub.scala
+++ src/test/scala/com/seanshubin/hello/FilesStub.scala
@@ -1,10 +1,10 @@
 package com.seanshubin.hello
 
-import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 
-class FilesStub(content: Map[String, String], charset: Charset) extends FilesContract {
+class FilesStub(content: Map[String, String]) extends FilesContract {
   override def readAllBytes(path: Path): Array[Byte] = {
-    content(path.toString).getBytes(charset)
+    content(path.toString).getBytes(StandardCharsets.UTF_8)
   }
 }
